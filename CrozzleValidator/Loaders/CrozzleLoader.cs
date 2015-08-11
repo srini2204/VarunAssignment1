45bf9375c226d11f667692eb454d20280eab05f8
Srinivas <srinivas@tkm9.com>
2015-08-11 17:36:48 +1000

    commit

--- CrozzleValidator/Loaders/CrozzleLoader.cs
+++ CrozzleValidator/Loaders/CrozzleLoader.cs
@@ -46,10 +46,24 @@ namespace CrozzleValidator
 
         private void validateNGenerateCrozzleData(string[] crozzleContent)
         {
-            char[,] data = new char[width, height];
+            char[][] data = new char[width][];
+
             for (int i = 0; i < crozzleContent.Length; i++)
             {
                 data[i] = crozzleContent[i].ToCharArray();
+            }
+
+            addMessage("Crozzle words retrieved from text file and populated in a 2D array");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (data[i][j] != ' ')
+                    {
+                        
+                    }
+                }
             }
         }
 
